5c720a8890fffd7aa1a7f977157bc32de18593b1
RIYAYADAV <TINKLEYADAV19@GMAIL.COM>
2025-02-15 13:04:48 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-blue-600">
+      Inital React and Tailwind Setup
+    </div>
+  );
+}
+
+export default App;
